5a5ba5d80708ad46670d7d76a69c47f821937a43
Phil <pflavin@gmail.com>
2015-08-20 11:08:11 +0100

    overwrote with my app!

--- leaderboard.js
+++ leaderboard.js
@@ -1,50 +1,148 @@
-// Set up a collection to contain player information. On the server,
-// it is backed by a MongoDB collection named "players".
+Profiles = new Mongo.Collection("profiles");
 
-Players = new Mongo.Collection("players");
+//Routes
 
+Router.route('/',{
+  template: '/login'
+});
+Router.route('/register');
+Router.route('/search');
+Router.route('/home');
+Router.route('/editProfile');
+Router.route('/messages');
+Router.route('/subscription');
+Router.route('/favourites');
+Router.route('/likesMe');
+Router.route('/lookedAt');
+Router.route('/lookedAtMe');
+Router.route('/logout');
+Router.route('/results' ,{
+  template: 'profiles'
+});
+Router.route('/profile/:_id', {
+    template: 'ProfilePage',
+    data: function(){
+        var currentProfile = this.params._id;
+        console.log("This is a profile page for ", currentProfile);
+        return Profiles.findOne({ _id: currentProfile});
+    }
+
+});
+
+
+//Helpers
+ 
 if (Meteor.isClient) {
-  Template.leaderboard.helpers({
-    players: function () {
-      return Players.find({}, { sort: { score: -1, name: 1 } });
-    },
-    selectedName: function () {
-      var player = Players.findOne(Session.get("selectedPlayer"));
-      return player && player.name;
+  // This code only runs on the client
+  Template.profiles.helpers({
+    profiles: function () {
+      console.log("This is a the profiles function.");
+      return Profiles.find({});
     }
   });
 
-  Template.leaderboard.events({
-    'click .inc': function () {
-      Players.update(Session.get("selectedPlayer"), {$inc: {score: 5}});
-    }
-  });
+//Events
 
-  Template.player.helpers({
-    selected: function () {
-      return Session.equals("selectedPlayer", this._id) ? "selected" : '';
+  
+Template.register.events({
+    'submit form': function(event){
+        event.preventDefault();
+        var username = $('[name=username').val();
+        var email = $('[name=email]').val();
+        var password = $('[name=password]').val();
+        Accounts.createUser({
+            username: username,
+            email: email,
+            password: password
+        }); 
+        Meteor.loginWithPassword(username,password,function(error){
+          if(error){
+            console.log(error.reason);
+          }else{
+            var d = new Date();
+            var n = d.toLocaleString();
+            Profiles.insert(
+              {createdBy: Meteor.userId(),
+              ProfileTitle: username, 
+              createdOn: n
+              });
+          }       
+        });        
+        Router.go('/editProfile');
     }
-  });
+});
+Template.home.events({
+    'click .logout': function(event){
+      console.log('Logout clicked');
+        event.preventDefault();
+        Meteor.logout();
+        Router.go('/');
+    }
 
-  Template.player.events({
-    'click': function () {
-      Session.set("selectedPlayer", this._id);
+});
+Template.login.events({
+    'submit form': function(event){
+      console.log('Login clicked');
+        event.preventDefault();
+        var email = $('[name=email]').val();
+        var password = $('[name=password]').val();
+        Meteor.loginWithPassword(email, password, function(error){
+          if(error){
+            console.log(error.reason);
+          } else {
+            var d = new Date();
+            var n = d.toLocaleString();
+            var profileDoc = Profiles.findOne({"createdBy" : Meteor.userId()});
+            Profiles.update({_id: profileDoc._id},
+              {$set:  {"lastLogIn" : n}}    
+            );
+          Router.go("/home");
+          }
+        }
+        );
+      }
+    });
+Template.user.helpers({
+  userName: function() {
+    return Meteor.user().username;
+  }
+});
+Template.editProfile.events({
+    'submit form': function(event){
+      console.log('Profile save');
+      //check that Profile belongs to current user.
+        var title = $('[name="ProfileTitle"]').val();
+        var gender = $('[name="gender"]').val();
+        var age = $('[name="age"]').val();
+        var location = $('[name="location"]').val();
+        var aboutMe = $('[name="aboutMe"]').val();
+        var exper=$('[name="experience"]').val();
+        var fee=$('[name="fees"]').val();
+        var profileDoc = Profiles.findOne({"createdBy" : Meteor.userId()});
+        console.log("Found:", profileDoc._id);
+        var d = new Date();
+        var n = d.toTimeString();
+        Profiles.update({_id: profileDoc._id},
+        {$set:
+        {
+          "ProfileTitle": title,
+          "gender": gender,
+          "location" : location,
+          "age": age,
+          "aboutMe": aboutMe,
+          "experience": exper,
+          "fees": fee,
+          "createdBy": Meteor.userId(),
+          "lastUpdate": n
+        }}
+        );
+        Router.go("/editProfile");
     }
-  });
-}
-
-// On server startup, create some players if the database is empty.
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    if (Players.find().count() === 0) {
-      var names = ["Ada Lovelace", "Grace Hopper", "Marie Curie",
-                   "Carl Friedrich Gauss", "Nikola Tesla", "Claude Shannon"];
-      _.each(names, function (name) {
-        Players.insert({
-          name: name,
-          score: Math.floor(Random.fraction() * 10) * 5
-        });
-      });
-    }
-  });
+});
+Template.editProfile.helpers({
+  'myProfile': function(){
+    console.log('myProfile helper function', Meteor.userId());
+        return Profiles.find({createdBy: Meteor.userId()});
+  }
+});
 }
