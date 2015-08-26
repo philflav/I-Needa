3a6084848db048ba16a0c9ffd80be7612a8b092f
Phil <pflavin@gmail.com>
2015-08-26 10:33:37 +0100

    Modals working

--- leaderboard.js
+++ leaderboard.js
@@ -131,9 +131,32 @@ if (Meteor.isClient) {
         createdOn: n
       });
       Router.go('/home');
-    }
-  })
+    },
+  });
 
+  Template.ProfilePage.events({
+    'click .send': function(event,tmpl) {
+      event.stopPropagation();
+      event.preventDefault();
+      console.log("Send button pressed");
+      var content = $('[name="myContent"]').val();
+      var recipientId = $('[name="recipientId"]').val();
+      var senderId = $('[name="sentFromId"]').val();
+      var recipientName = Profiles.findOne({createdBy: recipientId},{fields:{ProfileTitle: 1, _id:0}});
+      var senderName = Profiles.findOne({createdBy: senderId},{fields:{ProfileTitle: 1, _id:0}});
+      console.log("Sending to:" + (recipientName["ProfileTitle"])+ " from " + (senderName["ProfileTitle"]));
+      var d = new Date();
+      var n = d.toLocaleString();
+      Messages.insert({
+        sentToId: recipientId,
+        sentToName: (recipientName["ProfileTitle"]), 
+        sentFromId: senderId,
+        sentFromName: (senderName["ProfileTitle"]),
+          content: content,
+        createdOn: n
+      });
+    },
+  });
 
   Template.register.events({
     'submit form': function(event) {
@@ -215,6 +238,14 @@ if (Meteor.isClient) {
     }
   });
   Template.post.helpers({
+    userName: function() {
+      return Meteor.user().username;
+    },
+    userId: function() {
+      return Meteor.userId();
+    }
+  });
+    Template.ProfilePage.helpers({
     userName: function() {
       return Meteor.user().username;
     },
