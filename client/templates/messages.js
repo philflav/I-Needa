efa6422c1d7b3b2c27e9afa8a19da48b05ea58f9
Phil <pflavin@gmail.com>
2015-09-08 19:16:33 +0100

    Message deletion for sender and reciever implemented with database tidy up.

--- client/templates/messages.js
+++ client/templates/messages.js
@@ -2,31 +2,43 @@
  * Created by Phil on 01/09/2015.
  */
 Template.messages.helpers({
-    toMe: function() {
+    toMe: function () {
         console.log("Finding messages to the current user");
         return Messages.find({
-            sentToId: Meteor.userId()
-        }, {sort: { createdOn: -1 }}); //add date order sort
+            sentToId: Meteor.userId(), hideIn: 'no'
+        }, {sort: {createdOn: -1}}); //add date order sort
     },
-    fromMe: function() {
+    fromMe: function () {
         console.log("Finding messages from the current user");
         return Messages.find({
-            sentFromId: Meteor.userId()
-        }, {sort: { createdOn: -1 }}); //add date order sort
+            sentFromId: Meteor.userId(), hideOut: 'no'
+        }, {sort: {createdOn: -1}}); //add date order sort
+    },
+    messagesReceived: function () {
+        return Messages.find({
+            sentToId: Meteor.userId(), hideIn: 'no'
+        }).count();
+
+    },
+    messagesSent: function () {
+        return Messages.find({
+            sentFromId: Meteor.userId(), hideOut: 'no'
+        }).count();
+
     }
 });
 
 Template.messages.events({
-    'click .send': function(event,tmpl) {
+    'click .send': function (event, tmpl) {
         event.stopPropagation();
         event.preventDefault();
         console.log("Send button pressed");
         var content = $('[name="myContent"]').val();
         var recipientId = $('[name="recipientId"]').val();
         var senderId = $('[name="sentFromId"]').val();
-        var recipientName = Profiles.findOne({createdBy: recipientId},{fields:{ProfileTitle: 1, _id:0}});
-        var senderName = Profiles.findOne({createdBy: senderId},{fields:{ProfileTitle: 1, _id:0}});
-        console.log("Sending to:" + (recipientName["ProfileTitle"])+ " from " + (senderName["ProfileTitle"]));
+        var recipientName = Profiles.findOne({createdBy: recipientId}, {fields: {ProfileTitle: 1, _id: 0}});
+        var senderName = Profiles.findOne({createdBy: senderId}, {fields: {ProfileTitle: 1, _id: 0}});
+        console.log("Sending to:" + (recipientName["ProfileTitle"]) + " from " + (senderName["ProfileTitle"]));
         var d = new Date();
         var n = d.toLocaleString();
         Messages.insert({
@@ -35,11 +47,28 @@ Template.messages.events({
             sentFromId: senderId,
             sentFromName: (senderName["ProfileTitle"]),
             content: content,
+            "hideIn": "no",
+            "hideOut": "no",
             createdOn: n
         });
     },
 
-    "click button": function(event, template) {
+    "click button": function (event, template) {
         template.$("#myContent").toggle();
+    },
+    'click .deleteIn': function (event, tmpl) {
+        Messages.update(this._id, {$set: {hideIn: 'yes'}});
+        msg = Messages.findOne(this._id);
+        if (msg.hideOut == "yes")  {
+            Messages.remove(this._id);
+        }
+    },
+
+    'click .deleteOut': function (event, tmpl) {
+        Messages.update(this._id, {$set: {hideOut: 'yes'}});
+        msg = Messages.findOne(this._id);
+        if (msg.hideIn == "yes") {
+            Messages.remove(this._id);
+        }
     }
 });
